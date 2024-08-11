89ab9ac8b379d8dfdc0771c3017fb20e4e7da93b
EL MABROUK Marouane <elmabroukmarouane@gmail.com>
2024-08-11 02:53:48 +0200

    Add Shared Project

---
+++ JobOffers.FrontEnd.BackOffice.Shared/Components/Layout/MainLayout.razor.cs
@@ -0,0 +1,26 @@
+﻿using JobOffers.Frontend.Busines.Services.Interface;
+using JobOffers.Frontend.Domain.Settings;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace JobOffers.FrontEnd.BackOffice.Shared.Components.Layout
+{
+	public partial class MainLayout : IComponent
+    {
+        [Inject]
+        private IJSRuntime? JSRuntime { get; set; }
+        //[Inject]
+        //private ITitleService? TitleService { get; set; }
+        //[Inject]
+        //private BaseSettingsApp? BaseSettingApp { get; set; }
+        //protected override async Task OnInitializedAsync() => await TitleService!.SetTitlePage(BaseSettingApp!.BaseTitleApp);
+
+		protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (firstRender)
+            {
+                await JSRuntime!.InvokeVoidAsync("initAdminTemplate");
+            }
+        }
+    }
+}
--- JobOffers.Frontend.Domain/Settings/AppSettings.cs
+++ JobOffers.Frontend.Domain/Settings/AppSettings.cs
@@ -1,32 +1,43 @@
 ﻿namespace JobOffers.Frontend.Domain.Settings
 {
     // TODO : Make this class as singleton and create a json file loaded in program.cs or MauiProgram.cs in services to be configured via JSON file. In JSON File : Add as many objects as environment. Add also an attributes which will design the the chosen environment
-    public class BaseUrlApi
-    {
-        public required string BaseUrlApiWebHttp { get; set; }
+	// There are 3 environments : 
+	//		- Dev
+	//		- Test
+	//		- Prod
+    public class BaseSettingsApp
+	{
+		public required string ChosenEnviroment { get; set; }
+		public required string BaseUrlApiWebHttp { get; set; }
         public required string BaseUrlApiAndroidHttp { get; set; }
-        public BaseUrlApi GetBaseUrlsHttpDev()
-        {
-            return new() { 
-                BaseUrlApiAndroidHttp = "http://apigateway.local/",
-                BaseUrlApiWebHttp = "http://apigateway.local/"
-            };
-        }
-        public BaseUrlApi GetBaseUrlsHttpTest()
-        {
-            return new()
-            {
-                BaseUrlApiAndroidHttp = string.Empty,
-                BaseUrlApiWebHttp = string.Empty
-            };
-        }
-        public BaseUrlApi GetBaseUrlsHttpProd()
-        {
-            return new()
-            {
-                BaseUrlApiAndroidHttp = string.Empty,
-                BaseUrlApiWebHttp = string.Empty
-            };
-        }
+        public required string BaseTitleApp { get; set; }
+
+   //     public BaseSettingsApp GetBaseUrlsHttpDev()
+   //     {
+   //         return new() 
+   //         { 
+   //             ChosenEnviroment = "Dev",
+   //             BaseUrlApiAndroidHttp = "http://apigateway.local/",
+   //             BaseUrlApiWebHttp = "http://apigateway.local/"
+   //         };
+   //     }
+   //     public BaseSettingsApp GetBaseUrlsHttpTest()
+   //     {
+   //         return new()
+			//{
+			//	ChosenEnviroment = "Test",
+			//	BaseUrlApiAndroidHttp = string.Empty,
+   //             BaseUrlApiWebHttp = string.Empty
+   //         };
+   //     }
+   //     public BaseSettingsApp GetBaseUrlsHttpProd()
+   //     {
+   //         return new()
+			//{
+			//	ChosenEnviroment = "Prod",
+			//	BaseUrlApiAndroidHttp = string.Empty,
+   //             BaseUrlApiWebHttp = string.Empty
+   //         };
+   //     }
     }
 }
